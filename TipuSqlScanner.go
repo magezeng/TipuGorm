3bf3c6955e2ac464abc0fd3c7331662ebda2c45a
zengxianglin <429801517@qq.com>
2019-11-05 11:34:57 +0800

    转为gomod管理

--- TipuSqlScanner.go
+++ TipuSqlScanner.go
@@ -14,12 +14,12 @@ type TipuSqlScanner struct {
 
 func (scanner *TipuSqlScanner) Scan(src interface{}) error {
 	if srcString, ok := src.(string); ok {
-		return tipujson.StringToObjByReflect(srcString, scanner.Type, scanner.Value)
+		return tipujson.JsonStringToObjectByReflect(srcString, scanner.Type, scanner.Value)
 	}
 	tempPrint := fmt.Sprintf("%v", scanner.Type) + "   " + fmt.Sprintf("%v", scanner.Value)
 	fmt.Println(tempPrint)
 	if srcUint8Slice, ok := src.([]uint8); ok {
-		return tipujson.StringToObjByReflect(string(srcUint8Slice), scanner.Type, scanner.Value)
+		return tipujson.JsonStringToObjectByReflect(string(srcUint8Slice), scanner.Type, scanner.Value)
 	}
 	return errors.New("TipuSqlScanner 的src 必须为字符串")
 }
